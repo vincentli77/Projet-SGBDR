8e58eedcd8e124a7b20c6a5408d22375c6d03b57
Mehdi <mehdi@ublo.immo>
2022-06-21 11:43:58 +0200

    [Rename]: token function

--- backend/src/services/auth/jwt.service.ts
+++ backend/src/services/auth/jwt.service.ts
@@ -1,6 +1,4 @@
 import jwt from "jsonwebtoken";
 
-export const createToken = (email: string) => {
-	const expirationDate = new Date().setHours(new Date().getHours() + 1);
-	return jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: expirationDate });
+export const generateAccessToken = (email: string) => {
 };
