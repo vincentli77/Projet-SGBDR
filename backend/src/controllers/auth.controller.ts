40f6b09129107c36c04c1503f038aa918d51f46c
Mehdi <mehdi@ublo.immo>
2022-06-21 11:45:24 +0200

    [Add]: authenticateToken function

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,16 +1,13 @@
-import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { NextFunction, Request, Response } from "express";
 import { transport } from "../services/mail.service";
-import { createToken } from "../services/auth/jwt.service";
-import { isAuthenticated } from "./isAuthenticated";
+import { generateAccessToken } from "../services/auth/jwt.service";
 import { mailOptions } from "../config/mail.config";
+import { MailAuth } from "../interfaces/user.interface";
 
-interface mailAuth {
-	email: string;
-}
-
-export const mailAuth = async (req: Request<{}, {}, mailAuth>, res: Response): Promise<void> => {
+export const mailAuth = async (req: Request<{}, {}, MailAuth>, res: Response): Promise<void> => {
 	const body = req.body;
-	const token = createToken(body.email);
+	const token = generateAccessToken(body.email);
 
 	if (!body.email) {
 		res.status(404).send({
@@ -28,6 +25,24 @@ export const mailAuth = async (req: Request<{}, {}, mailAuth>, res: Response): P
 	});
 };
 
-export const connected = async (req: Request, res: Response): Promise<void> => {
-	isAuthenticated(req, res);
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+	const authHeader = req.headers["authorization"];
+	const token = authHeader && authHeader.split(" ")[1];
+
+	if (!token) {
+		res.status(403).send("Can't verify user.").redirect("/");
+		return;
+	}
+
+	jwt.verify(token, process.env.SECRET_KEY, (err: any) => {
+		
+		if (err) {
+			res.status(403).send("Invalid auth credentials.");
+			return;
+		}
+
+		console.log(req);
+
+		next();
+	});
 };
