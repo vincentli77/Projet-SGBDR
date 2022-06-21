fdba9106c20ee95315c4fd008f0e7ffd7e32bae0
Laura THAY <laura.thay@ipag.fr>
2022-06-21 17:50:01 +0200

    [add] style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
+import { Tab } from "./Components/Tab.component";
 import React from "react";
 import "./App.css";
-import { Header } from "./Components/Header.component";
-import { Row } from "./Components/Row.component";
 
 function App() {
 	return (
 		<div className="App">
-			App
-			<Header studentName="Name" email="email" score="score" />
-			<Row studentName="laura" email="lol" score={0} />
+			<div className="AppGlass">
+				<div></div>
+				<Tab studentName="laura" email="lol" score={20} />
+			</div>
 		</div>
 	);
 }
