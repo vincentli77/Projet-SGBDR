3d15961da3c7eb46b4f58388c075b28ff75d41ea
Vincent <li.jiajinvincent1997@gmail.com>
2022-06-21 19:31:30 +0200

    Add refresh token service

--- backend/src/services/auth/jwt.service.ts
+++ backend/src/services/auth/jwt.service.ts
@@ -3,3 +3,7 @@ import jwt from "jsonwebtoken";
 export const generateAccessToken = (email: string) => {
 	return jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: "15m" });
 };
+
+export const generateRefreshToken = (email: string) => {
+	return jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: "2 days" });
+};
