48e610b69fba7d4408e63c7b2cdebe410c204869
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2022-06-21 16:55:57 +0200

    [Add] Error messages

--- backend/src/controllers/ssh.controller.ts
+++ backend/src/controllers/ssh.controller.ts
@@ -4,13 +4,17 @@ import { checkUptime } from "../services/ssh.service";
 export const sshConnection = async (req: Request, res: Response): Promise<void> => {
   const { host, port, username } = req?.body ?? {};
 
-  // TODO: [urgent, not urgent] implement error message
-  if(!host || !port || !username) return;
+  if(!host || !port || !username) {
+    res.status(401).send(`Error : Missing inputs -> ${!host ? "host " : ""}${!port ? "port " : ""}${!username ? "username " : ""}`);
+    return;
+  }
 
-  const status = await checkUptime({ host, port, username })
+  const status = await checkUptime({ host, port, username });
+  
+  if(status.error) {
+    res.status(500).send(`Error : Wrong ssh credentials -> ${status.error}`);
+    return;
+  }
 
-  // If no status
-  if(!status) return
-
-  res.status(200).send('Yeah!')
+  res.status(200).send(status);
 };
