4f7d36bbef43ef3cfcf4d4e025a3c0f1767385e3
Mehdi <mehdi@ublo.immo>
2022-06-22 16:01:15 +0200

    [Fix]: eslint

--- backend/src/config/ssh.config.ts
+++ backend/src/config/ssh.config.ts
@@ -3,10 +3,10 @@ import path from "path";
 import { IUserConfig, IConfig } from "../interfaces/ssh";
 
 export const sshUserConfig = (config: IUserConfig): IConfig => {
-  return {
-    host: config.host,
-    port: config.port,
-    username: config.username,
-    privateKey: readFileSync(path.resolve(__dirname, "../../id_rsa")),
-  };
+	return {
+		host: config.host,
+		port: config.port,
+		username: config.username,
+		privateKey: readFileSync(path.resolve(__dirname, "../../id_rsa")),
+	};
 };
