b1628d055cc62bfbaf30fe70de47b007c9be28e3
Mehdi <mehdi@ublo.immo>
2022-06-22 15:47:17 +0200

    [Add]: expiration time on .env file

--- backend/src/services/auth/jwt.service.ts
+++ backend/src/services/auth/jwt.service.ts
@@ -1,9 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export const generateAccessToken = (email: string) => {
-	return jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: "15m" });
+	return jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN });
 };
 
 export const generateRefreshToken = (email: string) => {
-	return jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: "2 days" });
+	return jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN });
 };
