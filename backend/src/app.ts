d3a74e8eb2ea188751e28878cb8f68541165dc23
Mehdi <mehdi@ublo.immo>
2022-06-22 15:59:43 +0200

    [Del]: duplicate code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,6 @@ import express from "express";
 import logger from "morgan";
 
 import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require("dotenv").config();
 
 require("dotenv").config();
 
@@ -16,15 +14,11 @@ export const app = express();
 // Express configuration
 app.set("port", process.env.PORT || 3000);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-
 app.use(logger("dev"));
 
 // Set up middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded());
 app.use(cors());
 
 app.use("/", index);
