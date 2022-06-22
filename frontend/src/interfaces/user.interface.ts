c8287c4e935949281450cb479c7a87b52acfe848
Laura THAY <laura.thay@ipag.fr>
2022-06-22 12:10:15 +0200

    [add] user interface

---
+++ frontend/src/interfaces/user.interface.ts
@@ -0,0 +1,14 @@
+enum Role {
+	ADMIN = "admin",
+	STUDENT = "student",
+}
+
+export interface User {
+	id: string;
+	promoId: string;
+	email: string;
+	firstname: string;
+	lastname: string;
+	role: Role;
+	createdAt: string;
+}
