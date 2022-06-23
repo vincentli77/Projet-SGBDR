48d25ddee29fa6c6ff34a3872c602adad802f416
Vincent <li.jiajinvincent1997@gmail.com>
2022-06-23 14:08:13 +0200

    add find all users controller

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,4 +1,5 @@
 import { sshConnection } from "./ssh.controller";
 import { mailProvider, refreshToken, login } from "../controllers/auth.controller";
+import { findAllUsers } from "./dbquery.controller";
 
-export { mailProvider, refreshToken, login, sshConnection };
+export { mailProvider, refreshToken, login, sshConnection, findAllUsers };
