dc8885bc2dda83052d1c8c645d83b3d11f7264b5
Vincent <li.jiajinvincent1997@gmail.com>
2022-06-23 18:55:48 +0200

    Add crud function on controller

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,4 +1,4 @@
 import { sshConnection } from "./ssh.controller";
 import { mailProvider, refreshToken, login } from "../controllers/auth.controller";
-
-export { mailProvider, refreshToken, login, sshConnection };
+import { findAllUsers, createUsers, findAllUsersByPromo } from "./query.controller";
+export { mailProvider, refreshToken, login, sshConnection, findAllUsers, createUsers, findAllUsersByPromo };
