3f9714970c932332206f277cc7d15b59556f2128
Vincent <li.jiajinvincent1997@gmail.com>
2022-06-24 16:30:05 +0200

    Rename function

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,4 +1,4 @@
 import { sshConnection } from "./ssh.controller";
 import { mailProvider, refreshToken, login } from "../controllers/auth.controller";
-import { findAllUsers, createUsers, findAllUsersByPromo } from "./query.controller";
-export { mailProvider, refreshToken, login, sshConnection, findAllUsers, createUsers, findAllUsersByPromo };
+import { getUsers, createUser, getUsersPromo, challenges } from "./users.controller";
+export { mailProvider, refreshToken, login, sshConnection, getUsers, createUser, getUsersPromo, challenges };
