7497caa8aebddc1173015e94880d09815b89db85
Vincent <li.jiajinvincent1997@gmail.com>
2022-06-24 13:25:01 +0200

    Prevent injection sql from the query

--- backend/src/controllers/query.controller.ts
+++ backend/src/controllers/query.controller.ts
@@ -1,10 +1,11 @@
+import { log } from "console";
 import { Request, Response } from "express";
 import { Connect } from "../services/database.service";
 
 export const findAllUsers = async (req: Request, res: Response): Promise<void> => {
 	const db = await Connect();
 	try {
-		db.query("SELECT * FROM users", function (err: any, result: any) {
+		db.query("SELECT * FROM Users", function (err: any, result: any) {
 			if (err) throw err;
 			res.status(200).send({
 				result,
@@ -19,13 +20,19 @@ export const findAllUsers = async (req: Request, res: Response): Promise<void> =
 
 export const createUsers = async (req: Request, res: Response): Promise<void> => {
 	const db = await Connect();
-	const data = req.body;
-
+	const users = {
+		email: req.body.email,
+		first_name: req.body.first_name,
+		last_name: req.body.last_name,
+		role: req.body.role,
+	};
 	try {
 		db.query(
-			`INSERT INTO backendproject.users (email, first_name, last_name,role,promo_id) 
-             VALUES ('${data.email}', '${data.first_name}', '${data.last_name}','${data.role}',
-             (SELECT id FROM promotions WHERE promotions.name = '${data.promotion}'))`,
+			`INSERT INTO Users 
+			SET ?, promo_id  = (SELECT id 
+				FROM Promotions 
+				WHERE Promotions.name = ?)`,
+			[users, req.body.promotion],
 			function (err: any, result: any) {
 				if (err) throw err;
 				res.status(200).send({
@@ -43,19 +50,17 @@ export const createUsers = async (req: Request, res: Response): Promise<void> =>
 
 export const findAllUsersByPromo = async (req: Request, res: Response): Promise<void> => {
 	const db = await Connect();
-	const data = {
-		promotion: "P2021",
-	};
 	try {
 		db.query(
-			`SELECT * FROM users 
-             INNER JOIN promotions on '${data.promotion}' = promotions.name`,
+			`SELECT * FROM Users 
+             INNER JOIN Promotions on ? = Promotions.name`,
+			req.body.promotion,
 			function (err: any, result: any) {
 				if (err) throw err;
 				res.status(200).send({
 					result,
 				});
-			}
+			},
 		);
 	} catch (err) {
 		res.status(500).send({
@@ -66,7 +71,7 @@ export const findAllUsersByPromo = async (req: Request, res: Response): Promise<
 export const findAllChallenge = async (req: Request, res: Response): Promise<void> => {
 	const db = await Connect();
 	try {
-		db.query("SELECT * FROM challenges", function (err: any, result: any) {
+		db.query("SELECT * FROM Challenges", function (err: any, result: any) {
 			if (err) throw err;
 			res.status(200).send({
 				result,
