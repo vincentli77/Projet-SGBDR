164314bca0fd619d9e4665b052d7ccb7f43cb890
Vincent <li.jiajinvincent1997@gmail.com>
2022-06-24 17:10:22 +0200

    Add update score route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -16,5 +16,6 @@ index.get("/users", controller.getUsers);
 index.post("/user/create", controller.createUser);
 index.get("/usersPromo", controller.getUsersPromo);
 index.get("/challenges", controller.challenges);
+index.get("/updateScoreUser", controller.updateScoreUser);
 
 index.get("/");
