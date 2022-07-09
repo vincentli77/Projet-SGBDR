2c64d6748651d91ad911fc558dde57ee744f3c41
Mehdi <mehdi@ublo.immo>
2022-07-09 13:48:42 +0200

    [Add]: return type to tests functions

--- backend/src/tests/index.ts
+++ backend/src/tests/index.ts
@@ -2,30 +2,26 @@ import { IParams } from "../interfaces/ssh";
 import { sshService } from "../services/ssh-challenge01.service";
 
 import { Client } from "ssh2";
-
-interface ICallBack {
-	isSuccess: boolean;
-	error?: string;
-}
+import { EvaluationTestResult } from "../interfaces/evaluation.interface";
 
 const connection = new Client();
 
-export const verifyIfFileExistOnRemoteSession = (params: IParams) => {
+export const verifyIfFileExistOnRemoteSession = (params: IParams): EvaluationTestResult => {
 	return sshService(connection, "command", "result");
 };
 
-export const countJsFileOnRemoteSession = (params: IParams) => {
+export const countJsFileOnRemoteSession = (params: IParams): EvaluationTestResult => {
 	return sshService(connection, "command", "result");
 };
 
-export const convertInMsOnRemoteSession = (params: IParams) => {
+export const convertInMsOnRemoteSession = (params: IParams): EvaluationTestResult => {
 	return sshService(connection, "command", "result");
 };
 
-export const verifyIfIpIsValidOnRemoteSession = (params: IParams) => {
+export const verifyIfIpIsValidOnRemoteSession = (params: IParams): EvaluationTestResult => {
 	return sshService(connection, "command", "result");
 };
 
-export const findSmallestNumberOnRemoteSession = (params: IParams) => {
+export const findSmallestNumberOnRemoteSession = (params: IParams): EvaluationTestResult => {
 	return sshService(connection, "command", "result");
 };
