52b90762c2d3cdfbd73ddbcac496d838ddb77c54
Vincent <li.jiajinvincent1997@gmail.com>
2022-07-10 01:01:00 +0200

    token refresh api call

--- frontend/src/hook/useTokenRefresh.tsx
+++ frontend/src/hook/useTokenRefresh.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable prettier/prettier */
 export async function tokenRefresh(token) {
-
-	const response = await fetch("http://localhost:3333/refreshToken", {
+	const response = await fetch("http://localhost:3000/refreshToken", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
-			"Authorization": "Bearer " + token,
+			Authorization: "Bearer " + token,
 		},
 	});
-    const Alltoken = await response.json();
-    return Alltoken
-    
+	const Alltoken = await response.json();
+	return Alltoken;
 }
