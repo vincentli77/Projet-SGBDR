d47361591e526410f79ac2a2a46c9ed4ee8dd6de
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-07-10 23:10:23 +0200

    [Del]: so beautiful code omg

--- backend/src/tests/challenge/defaultTest.ts
+++
@@ -1,7 +0,0 @@
-import { EvaluationTestResult } from "../../interfaces/evaluation.interface";
-
-export const defaultTest = (callback: Promise<EvaluationTestResult>): void => {
-	it("Verify if hello.txt file exist on the remote session.", () => {
-		expect(callback).toBe({ isSuccess: true });
-	});
-};
