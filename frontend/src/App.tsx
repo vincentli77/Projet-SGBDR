3236aa7b6a36f35315a4809458c0f4f280f21b5d
Vincent <li.jiajinvincent1997@gmail.com>
2022-07-10 18:05:16 +0200

    add auth token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,12 @@
 import { Table } from "./Components/Table.component";
 import React from "react";
-import "./App.css";
 import { SideBar } from "./components/SideBar.component";
+import "./App.scss";
+import { Login } from "./Pages/Login/Login";
+import { checkToken } from "./hook/useCheckToken";
 
 function App() {
-	const [refreshToken, setRefreshToken] = useState(JSON.parse(sessionStorage.getItem("refresh_token")));
-	const [access_token, setAccessToken] = useState(JSON.parse(sessionStorage.getItem("access_token")));
-	const [check_token, setCheckToken] = useState(false);
-	const url = window.location;
-
-	useEffect(() => {
-		// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-		access_token ? null : setAccessToken(new URLSearchParams(url.search).get("token"));
-	}, []);
-
-	if (access_token) {
-		sessionStorage.setItem("access_token", JSON.stringify(access_token));
-	}
-
-	if (access_token && !refreshToken) {
-		const Alltoken = tokenRefresh(access_token);
-		Alltoken.then(function (result) {
-			setRefreshToken(result.refreshToken);
-			sessionStorage.setItem("refresh_token", JSON.stringify(result.refreshToken));
-			setCheckToken(true);
-		});
-	}
-
-	if (access_token && refreshToken) {
-		const newAccessToken = accessRefresh(refreshToken);
-		newAccessToken.then(function (result) {
-			if (result.accessToken) {
-				sessionStorage.setItem("access_token", JSON.stringify(result.accessToken));
-				setCheckToken(true);
-			} else {
-				setCheckToken(false);
-			}
-		});
-	}
-
+	const check_token = checkToken();
 	if (!check_token) {
 		return <Login />;
 	}
@@ -46,6 +14,9 @@ function App() {
 		<div className="App">
 			<div className="AppGlass">
 				<SideBar />
+			<p>connected</p>
+			<div className="AppGlass">
+				<Table users={[]} />
 			</div>
 		</div>
 	);
