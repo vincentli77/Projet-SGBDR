913cd6aac85fd757594d05cf1ea5508c44b65a79
Verfaillie Mehdi <verfailliemehdi@imac-de-verfaillie.home>
2022-07-10 17:43:19 +0200

    [Add]: sequencerService

---
+++ backend/src/services/sequencer.service.ts
@@ -0,0 +1,11 @@
+import { interpret } from "xstate";
+import { SshUserConfig } from "../interfaces/ssh";
+import { sequencer } from "../helpers/tests.sequencer";
+import { stateMachineContext } from "../helpers/tests.sequencer";
+
+export const sequencerService = (config: SshUserConfig) =>
+	interpret(sequencer.withContext({ ...stateMachineContext, ssh: config }))
+		.onTransition((state) => console.log("ON_TRANSITION: ", state.context))
+		.onEvent((listener) => console.log("ON_EVENT: ", listener))
+		.onDone((listener) => console.log("ON_DONE: ", listener))
+		.start();
