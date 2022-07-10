fc3c625a5a3320644ad31f5e4b2bb18ecabbe4b4
Vincent <li.jiajinvincent1997@gmail.com>
2022-07-10 17:46:31 +0200

    change port

--- frontend/src/hook/useTokenRefresh.tsx
+++ frontend/src/hook/useTokenRefresh.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 export async function tokenRefresh(token) {
 
-	const response = await fetch("http://localhost:3000/refreshToken", {
+	const response = await fetch("http://localhost:3333/refreshToken", {
 		method: "POST",
 		headers: {
 			"Content-Type": "application/json",
