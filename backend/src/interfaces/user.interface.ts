5097d090815cc709b3724e2ded08d5050de459c2
Mehdi <mehdi@ublo.immo>
2022-07-11 10:58:33 +0200

    [Add]: missing interface

--- backend/src/interfaces/user.interface.ts
+++ backend/src/interfaces/user.interface.ts
@@ -2,3 +2,22 @@
 export interface MailProvider {
 	email: string;
 }
+
+export interface User {
+	id: string;
+	promoId: string;
+	email: string;
+	firstname: string;
+	lastname: string;
+	role: "admin" | "student";
+	createdAt: string;
+}
+
+export interface UserChallengeResult {
+	id: string;
+	score: number;
+	userId: string;
+	challengeId: string;
+	promoId: string;
+	createdAt: string;
+}
