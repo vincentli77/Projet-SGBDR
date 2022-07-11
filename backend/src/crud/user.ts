371b6e27fad9ad05404bb7fcc0a932e0aa06ee3a
Vincent <li.jiajinvincent1997@gmail.com>
2022-07-11 15:56:57 +0200

    sql fix promotion name

--- backend/src/crud/user.ts
+++ backend/src/crud/user.ts
@@ -2,12 +2,12 @@
  *
  */
 export const getUsers = "SELECT * FROM Users";
-export const getUsersByPromotionName = `SELECT * FROM Users 
-             INNER JOIN Promotions on ? = Promotions.name`;
 
-/**
- *
- */
+export const getUsersByPromotionName = `SELECT * FROM Users  WHERE 
+promo_id = (SELECT id FROM Promotions WHERE Promotions.name = ?)`;
+
+
+ 
 export const createUser = `INSERT INTO Users 
 			SET ?, promo_id  = (SELECT id 
 				FROM Promotions 
