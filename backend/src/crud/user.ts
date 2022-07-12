f957838e4d89a178e1776da1e6b3ce4ff41493c6
Vincent <li.jiajinvincent1997@gmail.com>
2022-07-12 13:09:14 +0200

    rangement crud

--- backend/src/crud/user.ts
+++ backend/src/crud/user.ts
@@ -2,7 +2,6 @@
  *
  */
 export const getUsers = "SELECT * FROM Users";
-
 export const getUsersByPromotionName = `SELECT * FROM Users  WHERE 
 promo_id = (SELECT id FROM Promotions WHERE Promotions.name = ?)`;
 
@@ -12,12 +11,6 @@ export const createUser = `INSERT INTO Users
 			SET ?, promo_id  = (SELECT id 
 				FROM Promotions 
 				WHERE Promotions.name = ?)`;
-
-/**
- *
- */
-export const updateUserScore = "UPDATE Results SET score=? WHERE user_id=? AND challenge_id=?";
-
 export const createResult = `INSERT INTO Results 
 			SET user_id  = (SELECT id 
 				FROM Users 
@@ -29,6 +22,11 @@ export const createResult = `INSERT INTO Results
 				FROM Promotions 
 				WHERE Promotions.name = ?),
 				score = 0`;
+
+/**
+ *
+ */
+export const updateUserScore = "UPDATE Results SET score=? WHERE user_id=? AND challenge_id=?";
 
 
 
