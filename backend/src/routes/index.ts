8bf48c8c2b2e6151671e8a148e7ba901572a733a
Mehdi <mehdi@ublo.immo>
2022-07-13 13:20:30 +0200

    [Fix]: routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -10,14 +10,13 @@ index.post("/evaluation", controller.evaluation);
 index.post("/mailProvider", controller.mailProvider);
 index.post("/refreshToken", controller.refreshToken);
 index.get("/accessToken", controller.accessToken);
-
-//crud users
-index.post("/user", controller.getUserByMail);
 index.post("/user/create", controller.createUser);
 index.post("/result/create", controller.createResult);
+//crud users
 
-index.get("/users", controller.getUsers);
 index.get("/result", controller.getUserResult);
+index.get("/users", controller.getUsers);
+index.get("/user", controller.getUserByMail);
 index.get("/promotions", controller.getPromotions);
 index.get("/usersPromo", controller.getUsersByPromotionName);
 index.get("/challenges", controller.getChallenges);
